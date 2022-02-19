99fc922016b1191513368b05a6c59dc4683d5739
ozlem55 <65675763+ozlem55@users.noreply.github.com>
2022-02-19 16:11:33 +0300

    Initial commit

---
+++ Whıle-Foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Whıle_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1den başlayarak consoleden girilen sayıya kadar ortalama hesaplauıp console e yazdıran program
+            Console.WriteLine("Lütfen bir sayı giriniz.");
+            int sayi= int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            Console.WriteLine("************************");
+
+            //a'dan z'ye kadar bütün harfleri conseleye yazdır.
+            char character = 'a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            Console.WriteLine("************************");
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
